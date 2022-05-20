a2752f3fb9639f81b18719fa859d6efd1b5059ba
tigranv4 <tigranv4@gmail.com>
2022-05-20 10:29:06 +0300

    TigranSession1

---
+++ BookStore/DataBase/BookPartial.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DataBase
+{
+    public partial class Book
+    {
+        public string UpdatedCost
+        {
+            get
+            {
+                if(App.dbcontext.MaxDiscount.Where(c => c.Id == SizeMaxDiscount).First().MaxDiscount1 > 0)
+                {
+                    double k = (Convert.ToDouble(Cost) * Convert.ToDouble(CurrentDiscount)) / 100;
+                    double res = Convert.ToDouble(Cost) - k;
+                    return res.ToString();
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+        public bool IsDescointed
+        {
+            get 
+            {
+                if (string.IsNullOrEmpty(CurrentDiscount.ToString()))
+                {
+                    return false;
+                }
+                else 
+                {
+                    return true;
+                }
+            }
+        }
+        public string UpdateColor
+        {
+            get
+            {
+                if (App.dbcontext.MaxDiscount.Where(c => c.Id == SizeMaxDiscount).First().MaxDiscount1 > 15)
+                {
+                    return "#7fff00";
+                }
+                else
+                {
+                    return "Transparent";
+                }
+            }
+        }
+
+        public string IfDiscount
+        {
+            get
+            {
+                if (CurrentDiscount > 0)
+                {
+                    return "Strikethrough";
+                }
+                else
+                    return "None";
+            }
+        }
+    }
+}
---
+++ BookStore/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BookStore.DataBase;
+
+namespace BookStore
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Book> books =  App.dbcontext.Book.ToList();
+        public MainWindow()
+        {
+            for (int i = 0; i < books.Count; i++)
+            {
+                if (books[i].Image == null || books[i].Image.Length == 0)
+                    books[i].Image = "Resourses/picture.png";
+            }
+            InitializeComponent();
+            ListBook.ItemsSource = books;
+
+            
+        }
+    }
+}
